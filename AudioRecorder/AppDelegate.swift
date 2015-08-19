6c3a2cc74b5254265fe8ba2a2fb338c7fe7214ad
Justin Winter <justinlevi@gmail.com>
2015-08-19 19:35:08 -0400

    swift 2 update

--- AudioRecorder/AppDelegate.swift
+++ AudioRecorder/AppDelegate.swift
@@ -10,18 +10,10 @@ import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
-    @IBOutlet var window: NSWindow
+    @IBOutlet var window: NSWindow!
 
 
-    func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(aNotification: NSNotification?) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication!) -> Bool {
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
         return true
     }
 
